e346dde738e92406131c555a124a82a32609dc4c
Vladimir Shvab <shvab.vladimir@gmail.com>
2024-09-20 00:19:28 +0300

    add startfilter favorite

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,17 @@
-from django_filters import BooleanFilter, FilterSet
+from django_filters.rest_framework import FilterSet, filters
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
-    is_favorited = BooleanFilter(field_name='is_favorite')
+    is_favorited = filters.BooleanFilter(
+        method='is_favorited_filter')
 
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author',)
+
+    def is_favorited_filter(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorites=user)
+        return queryset
