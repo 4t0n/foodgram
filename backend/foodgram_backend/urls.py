aee19d95d35e2f40d6172734c8414689dc567de8
Vladimir Shvab <shvab.vladimir@gmail.com>
2024-09-30 01:37:32 +0300

    fix flake8

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.defaults import page_not_found
 from django.views.generic import TemplateView
 
 from api.views import ShortLinkRedirect, about_technologies
